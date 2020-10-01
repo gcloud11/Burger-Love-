99bc1df83b7578021c5d294a33e8a4092671c783
Gayla Cloud <gaylacloud@Gaylas-MacBook-Pro.local>
2020-09-30 19:05:57 -0500

    commit 1. Finished all folders required.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Select burger database --
+USE burgers_db;
+-- Insert rows --
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Good Burger", FALSE), ("Mondo Bueger", FALSE), ("Ham & Cheese Burger", FALSE);
